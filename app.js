232ac3b5cde6aac5053348d107373bdac0f403b6
Abdulbasit Gamoranao <basitgamor@gmail.com>
2021-06-26 18:55:52 +0800

    Update with Async/await

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = new EasyHTTP();
 
 // Get Posts
-http
-  .get("https://jsonplaceholder.typicode.com/users")
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// http
+//   .get("https://jsonplaceholder.typicode.com/users")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
 
 // Create Data
 const data = {
@@ -14,16 +14,16 @@ const data = {
 };
 
 // Create Post
-http
-  .post("https://jsonplaceholder.typicode.com/users", data)
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// http
+//   .post("https://jsonplaceholder.typicode.com/users", data)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
 
 // Update Post
-http
-  .put("https://jsonplaceholder.typicode.com/users/2", data)
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// http
+//   .put("https://jsonplaceholder.typicode.com/users/2", data)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
 
 // Delete Post
 http
